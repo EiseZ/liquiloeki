25dfc09a3f173c691e4c5e4c9f4c5997f1e92ffa
EiseZ <eise.zimmerman@outlook.com>
2022-01-11 18:05:55 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let score = 0;
+let time = 20;
+let firstTime = true;
+let gameEnded = false;
+function imgclicked() {
+    if (gameEnded) return;
+    if (firstTime) {
+        setInterval(decreaseTime, 1000);
+        firstTime = false;
+    }
+    score++;
+    let scoreText = document.getElementById("text");
+    scoreText.innerHTML = "Score: " + score.toString();
+    let img = document.getElementById("loeki");
+    img.setAttribute("draggable", false);
+    img.style.top =
+        Math.floor(Math.random() * (window.innerHeight - 100)).toString() +
+        "px";
+    img.style.left =
+        Math.floor(Math.random() * (window.innerWidth - 100)).toString() + "px";
+}
+
+function decreaseTime() {
+    time--;
+    if (time <= 0) endGame();
+    let timeText = document.getElementById("time");
+    timeText.innerHTML = "Tijd: " + time.toString();
+}
+
+function endGame() {
+    gameEnded = true;
+    let scoreText = document.getElementById("text");
+    let timeText = document.getElementById("time");
+    let img = document.getElementById("loeki");
+    timeText.remove();
+    if (score >= 40) {
+        scoreText.innerHTML = "Wow, je heb Loeki vermoord!";
+    } else {
+        scoreText.innerHTML = "AH, jammer, Loeki is ontsnapt";
+        img.src = "img/defeat.jpg";
+    }
+}
